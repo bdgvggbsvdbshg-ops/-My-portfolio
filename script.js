15950b99539c5de1a2a3c46fa9cf6f4783e1fbaa
bdgvggbsvdbshg-ops <bdgvggbsvdbshg@gmail.com>
2026-07-17 08:27:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Mai Văn Hoài Nam đã load xong!');
+    
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+    });
