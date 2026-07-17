5b4f990c5ff3139dba0336885fb52554e0bfafb3
bdgvggbsvdbshg-ops <bdgvggbsvdbshg@gmail.com>
2026-07-17 08:36:55 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio của Mai Văn Hoài Nam đã load xong!');
-    
-    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
-    });
